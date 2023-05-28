c276f0f2d31bc1866420a0571b0c891b028e11b2
Shipan <liushipan1998@gmail.com>
2023-05-28 23:57:37 +0200

    complex query done

--- django_projects/bookmanager/bookstore/views.py
+++ django_projects/bookmanager/bookstore/views.py
@@ -4,5 +4,14 @@ from .models import Book
 def all_books(request):
     books = Book.objects.all();
     #go to the html file and bring some data
-    return render(request, "bookstore/01_all_book.html", locals())
+
+    dict = {
+        "books": books
+    }
+
+    # 使用 locals()
+    # return render(request, "bookstore/01_all_book.html", locals())
+
+    # 使用自己封装的 方式
+    return render(request, "bookstore/01_all_book.html", dict)
 
